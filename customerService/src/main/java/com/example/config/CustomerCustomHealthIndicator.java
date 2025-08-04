a1a4508e751a72532fd220834bb38f63895aef7e
1194314874@qq.com <zeng123456>
2025-08-04 14:39:51 +0800

    添加：健康指示器

---
+++ customerService/src/main/java/com/example/config/CustomerCustomHealthIndicator.java
@@ -0,0 +1,15 @@
+package com.example.config;
+
+import org.springframework.boot.actuate.health.AbstractHealthIndicator;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.stereotype.Component;
+
+// 类名必须是XxxxxHealthIndicator
+@Component
+public class CustomerCustomHealthIndicator extends AbstractHealthIndicator {
+    @Override
+    protected void doHealthCheck(Health.Builder builder) throws Exception {
+        builder.up().withDetail("消费者自定义健康指示器", "浙江宏振智能芯片有限公司成立于2019年11月，法定代表人为祝丽丹，注册资本1000万人民币，经营范围包括集成电路、电路模块、软...")
+        .withDetail("key1","value1");
+    }
+}
---
+++ providerService/src/main/java/com/example/config/ProviderCustomHealthIndicator.java
@@ -0,0 +1,13 @@
+package com.example.config;
+
+import org.springframework.boot.actuate.health.AbstractHealthIndicator;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProviderCustomHealthIndicator extends AbstractHealthIndicator {
+    @Override
+    protected void doHealthCheck(Health.Builder builder) throws Exception {
+        builder.up().withDetail("提供者自定义健康指示器", "provider service 服务启动正常！");
+    }
+}
