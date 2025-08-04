2c2460e3be401ee3d7bcb5861c375cc85c509449
1194314874@qq.com <zeng123456>
2025-08-04 14:26:44 +0800

    添加：信息

--- customerService/src/main/java/com/example/config/CustomerInfo.java
+++ customerService/src/main/java/com/example/config/CustomerInfo.java
@@ -2,6 +2,8 @@ package com.example.config;
 
 import org.springframework.boot.actuate.info.Info;
 import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.boot.info.JavaInfo;
+import org.springframework.boot.info.OsInfo;
 import org.springframework.stereotype.Component;
 
 import java.text.SimpleDateFormat;
@@ -12,14 +14,23 @@ import java.util.Map;
 @Component
 public class CustomerInfo implements InfoContributor {
 
+    private JavaInfo javaInfo;
+    private OsInfo osInfo;
+
+    public CustomerInfo() {
+        this.javaInfo = new JavaInfo();
+        this.osInfo = new OsInfo();
+    }
+
     @Override
     public void contribute(Info.Builder builder) {
         Map<String, Object> map = new HashMap<>();
         map.put("贡献者", "衍育");
-        map.put("artifactId", "customer-service");
         map.put("服务名称", "customer-service");
         map.put("描述", "Service Consumer");
         map.put("当前日期", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+        map.put("java信息", this.javaInfo);
+        map.put("操作系统", this.osInfo);
         builder.withDetails(map);
     }
 
--- providerService/src/main/java/com/example/config/ProviderInfo.java
+++ providerService/src/main/java/com/example/config/ProviderInfo.java
@@ -1,7 +1,9 @@
 package com.example.config;
 
-import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.Info.Builder;
 import org.springframework.boot.actuate.info.InfoContributor;
+import org.springframework.boot.info.JavaInfo;
+import org.springframework.boot.info.OsInfo;
 import org.springframework.stereotype.Component;
 
 import java.text.SimpleDateFormat;
@@ -12,14 +14,22 @@ import java.util.Map;
 @Component
 public class ProviderInfo implements InfoContributor {
 
+    private JavaInfo javaInfo;
+    private OsInfo osInfo;
+
+    public ProviderInfo() {
+        this.javaInfo = new JavaInfo();
+        this.osInfo = new OsInfo();
+    }
+
     @Override
-    public void contribute(Info.Builder builder) {
+    public void contribute(Builder builder) {
         Map<String, Object> map = new HashMap<>();
         map.put("贡献者", "衍育");
-        map.put("artifactId", "provider-service");
         map.put("服务名称", "provider-service");
-        map.put("描述", "Service Provider");
         map.put("当前日期", new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+        map.put("java信息", this.javaInfo);
+        map.put("操作系统", this.osInfo);
         builder.withDetails(map);
     }
 
